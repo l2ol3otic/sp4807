ebd5c474dac00d2a82a2b89fa0f811cf010d7454
l2ol3otic <l2ol3otic@gmail.com>
2018-07-05 18:39:23 +0700

    login

--- src/app/Components/login/login.component.ts
+++ src/app/Components/login/login.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { AppURL } from 'src/app/app.url';
+import { IloginComponent } from './login.interface';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements IloginComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private builder:FormBuilder,
+    private router: Router
+  ) {
+    this.initialCreateFormData();
+   }
+  Url = AppURL;
+  form: FormGroup
+  //ขเ้าสู่ระบบ
+  onSubmit(): void {
+    console.log(this.form.value);
+    if(this.form.invalid){
+      console.log("error")
+    }else{
+      this.router.navigate(['/', AppURL.Authen])
+    }
+    
   }
+  private initialCreateFormData(){
 
+    this.form = this.builder.group({
+      username: ['',Validators.required],
+      password: [],
+      remember: []
+    });
+  }
 }
---
+++ src/app/Components/login/login.interface.ts
@@ -0,0 +1,7 @@
+import { FormGroup } from "@angular/forms";
+
+export interface IloginComponent{
+    Url:any;
+    form:FormGroup
+    onSubmit():void; 
+}
